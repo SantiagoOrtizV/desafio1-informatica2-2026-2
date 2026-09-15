9602baf0b702d4a2d93f46a30cb2bd4759fd76b7
Santiago Ortiz <s.ortiz2@udea.edu.co>
2026-09-15 03:11:36 -0500

    Implementacion estructura dinamica e impresion del tablero

--- main.cpp
+++ main.cpp
@@ -3,26 +3,30 @@
 
 using namespace std;
 
-int main()
-{
+int main(){
     bool salir_inicio = false, salir_juego;
     unsigned short int opc;
-    unsigned int* ptr = nullptr;
+    char* ptr = nullptr;
+    unsigned int bytes_reservados;
 
     while(!salir_inicio){
         cout << "BIENVENIDO A SWEET CRUSH" << endl;
         cout << "1) Iniciar juego" << endl;
         cout << "2) Salir" << endl << endl;
-        cout << "Ingrese una opcion: " << endl;
+        cout << "Ingrese una opcion: ";
         cin >> opc;
+        cout << endl;
+
         switch(opc){
         case 1:
-            unsigned int m,n;
+            unsigned int c,f;
             salir_juego = false;
-            inicializacion_juego(m, n, ptr);
+            inicializacion_juego(c, f, ptr, bytes_reservados);
             while(!salir_juego){
-                turno(salir_juego, m, n, ptr);
+                //turno(salir_juego, c, f, ptr, bytes_reservados);
             }
+            delete ptr;
+            ptr = nullptr;
             break;
 
         case 2:
