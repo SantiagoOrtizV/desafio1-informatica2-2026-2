03d3a21e4f28fc177b8c76412f6e724d3e0fc8ef
Santiago Ortiz <s.ortiz2@udea.edu.co>
2026-09-20 20:52:44 -0500

    Limpieza de comentarios

--- funciones.cpp
+++ funciones.cpp
@@ -13,43 +13,26 @@ char generar_ficha(unsigned short int i, unsigned short int f){
 }
 
 void setFicha(unsigned char* arreglo, int indice, unsigned char valor){
-    // 1. Limpiamos el valor para asegurar que solo contenga datos en sus 3 bits menos significativos
-    valor = valor & 0x07; // 0x07 es 00000111 en binario
-
-    // 2. Calculamos la posición exacta en el arreglo
+    valor = valor & 0x07;
     int bit_inicial = indice * 3;
     int byte_idx = bit_inicial / 8;
-    int bit_offset = bit_inicial % 8; // Desplazamiento desde la izquierda (MSB)
+    int bit_offset = bit_inicial % 8;
 
-    // Caso A: El elemento cabe completamente dentro de un solo byte
-    // Si el offset es 5 o menos, quedan al menos 3 bits libres en este byte
     if (bit_offset <= 5) {
-        // Calculamos cuánto debemos desplazar el valor hacia la izquierda
+
         int shift = 5 - bit_offset;
-
-        // Creamos una máscara para poner en 0 solo los 3 bits que vamos a modificar
         unsigned char mascara = ~(0x07 << shift);
-
-        // Aplicamos la máscara y escribimos el nuevo valor
         arreglo[byte_idx] = (arreglo[byte_idx] & mascara) | (valor << shift);
     }
-    // Caso B: El elemento está partido entre el byte actual y el siguiente
     else {
-        // ¿Cuántos bits caben en el primer byte y cuántos sobran para el segundo?
         int bits_en_primero = 8 - bit_offset;
         int bits_en_segundo = 3 - bits_en_primero;
 
-        // --- Modificamos el PRIMER byte ---
-        // Máscara para borrar los últimos 'bits_en_primero' bits del byte actual
         unsigned char mascara1 = ~((1 << bits_en_primero) - 1);
-        // Desplazamos el valor a la derecha para quedarnos con su parte más significativa
         arreglo[byte_idx] = (arreglo[byte_idx] & mascara1) | (valor >> bits_en_segundo);
 
-        // --- Modificamos el SEGUNDO byte ---
-        // Máscara para borrar los primeros 'bits_en_segundo' bits del siguiente byte
         int shift2 = 8 - bits_en_segundo;
         unsigned char mascara2 = ~(((1 << bits_en_segundo) - 1) << shift2);
-        // Extraemos solo los bits que faltan escribir y los empujamos a la izquierda del nuevo byte
         unsigned char valor_resto = valor & ((1 << bits_en_segundo) - 1);
         arreglo[byte_idx + 1] = (arreglo[byte_idx + 1] & mascara2) | (valor_resto << shift2);
     }
